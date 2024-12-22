f095a821d9ee04aae513e3240df1069187d18cb5
divya-thota <divyathota1208@gmail.com>
2024-12-22 10:29:33 -0500

    Spring boot initial commit

---
+++ finance_tracker_service/src/main/java/com/personal/finance_tracker_service/FinanceTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.personal.finance_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerServiceApplication.class, args);
+	}
+
+}
